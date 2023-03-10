067534d716dffbd042a4b1764032367cc6997439
MagrelliJoe <magrelli1988@gmail.com>
2023-03-10 02:51:58 +0100

    crea classi entities e implementa i primi metodi del Repository

--- src/main/java/model/data/implementation/RepositotyP.java
+++ src/main/java/model/data/implementation/RepositotyP.java
@@ -13,7 +13,7 @@ public class RepositotyP implements Repository {
 
     @Override
     public void chooseAttack(Pet pet_att, Pet pet_def) {
-        if(pet_att.getSpeed() > pet_def.getSpeed()){
+        if( pet_att.getSpeed() > pet_def.getSpeed()){
             attack(pet_att,pet_def);
         }else{
             attack(pet_def,pet_att);
